48a33168a8c865a62874761f39ba1d752eb01d06
Smilek <smilek@ubuntu.ubuntu-domain>
2010-11-30 21:42:58 +0300

    some_auth

---
+++ ishop/app/controllers/user_session_controller.rb
@@ -0,0 +1,2 @@
+class UserSessionController < ApplicationController
+end
--- ishop/db/schema.rb
+++ ishop/db/schema.rb
@@ -9,6 +9,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20101130183409) do
+
+  create_table "user_sessions", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
